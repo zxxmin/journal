64c80ebfbadd667494769f379f100da13350a371
songjimin <ssong503503@gmail.com>
2024-09-12 09:28:22 +0900

    feat: journal 구조 다시 잡기 + 초기 세팅 + 하던것은 journal_old로 변경

--- journal_old/src/main.jsx
+++ journal_old/src/main.jsx
@@ -1,10 +1,11 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowerRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowerRouter>
-    <App />
-  </BrowerRouter>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
