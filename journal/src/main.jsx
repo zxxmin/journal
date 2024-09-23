66f07ad9f1a7d7bf6416ee4851ed8e6fb4d6adf8
songjimin <ssong503503@gmail.com>
2024-09-23 13:20:17 +0900

    BrowserRouter를 HashRouter로 변경

--- journal/src/main.jsx
+++ journal/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/journal/'>
-      <App />
-  </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
